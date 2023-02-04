3bd5c0a5f4a7bb4b86415ab5b325a1c278623adb
Donnadonnam <123767522+Donnadonnam@users.noreply.github.com>
2023-02-03 20:17:12 -0600

    Donnadonnam/ConfigControllerTest.java - narcos [Git...github.dev

--- config/src/test/java/com/alibaba/nacos/config/server/controller/ConfigControllerTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/controller/ConfigControllerTest.java
@@ -1,18 +1,11 @@
-/*
- * Copyright 1999-2018 Alibaba Group Holding Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+#9880/*
+#4981 *
+#7099 * you may not use this file except in compliance with the License.
+#9821 *
+#9821 *
+#9848 * distributed under the License is distributed on an "AS IS" BASIS,
+#9848 * See the License for the specific language governing permissions and
+#9848 */
 
 package com.alibaba.nacos.config.server.controller;
 
